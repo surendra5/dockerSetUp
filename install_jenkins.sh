5e3e1a304a4b124c9ba705989612350ae808897d
Surendra Meena <surendrameena5@gmail.com>
2019-08-25 19:04:17 +0530

    Update install_jenkins.sh

--- install_jenkins.sh
+++ install_jenkins.sh
@@ -3,6 +3,9 @@
 # this script is only tested on ubuntu xenial
 
 # install docker
+
+sudo apt-key adv --keyserver hkp://p80.pool.sks-keyservers.net:80 --recv-keys 58118E89F3A912897C070ADBF76221572C52609D
+sudo apt-add-repository 'deb https://apt.dockerproject.org/repo ubuntu-xenial main'
 sudo apt-get update
 sudo apt-get install -y docker-engine
 sudo systemctl enable docker
