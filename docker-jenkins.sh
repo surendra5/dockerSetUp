b4dc1b86da7517f420d2e41f22d766b3bbcc5a6d
Surendra Meena <surendrameena5@gmail.com>
2019-08-25 18:38:03 +0530

    Add files via upload

---
+++ docker-jenkins.sh
@@ -0,0 +1,23 @@
+#docker install script for ubuntu xenial 16.04
+
+#install docker
+apt-get remove docker docker-engine docker.io containerd runc
+apt-get update
+apt-get install apt-transport-https ca-certificates curl gnupg-agent software-properties-common
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+apt-key fingerprint 0EBFCD88
+add-apt-repository  "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
+apt-get update
+apt-get install docker-ce docker-ce-cli containerd.io
+docker run hello-world
+
+#docker endpoint
+echo 'Docker installed Successfully'
+
+#run jenkins
+mkdir -p /var/jenkins_home
+chown -R 1000:1000 /var/jenkins_home/
+docker run -p 8080:8080 -p 50000:50000 -v /var/jenkins_home:/var/jenkins_home -d --name jenkins jenkins/jenkins:lts
+
+#jenkins endpoint
+echo 'jenkins install successfully, access from http://'$(curl -s ifconfig.co)':8080'
